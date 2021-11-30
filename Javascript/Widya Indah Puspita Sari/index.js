b020411bcfc0efeeae5b0b061daafdcfba6567a1
WidyaIn <91048154+WidyaIn@users.noreply.github.com>
2021-11-30 20:52:39 +0700

    Pertemuan 4

---
+++ Javascript/Widya Indah Puspita Sari/index.js
@@ -0,0 +1,15 @@
+// alert memunculkan kalimat di dalam kurung seperti pemberitahuan. jadi sebelum masuk ke halaman utama
+// akan tampil alert tersebut. dan harus klik "ok" agar dapat masuk ke halaman utama
+alert('Selamat Belajar Alert di Javascript');
+alert('Selamat Malam');
+
+//jika alert hanya menampilkan kalimat yang terdapat di dalam kurung
+// jika prompt mengisikan(inputan). Kemudian dimasukkan ke variabel agar dapat di munculkan
+var nama = prompt("Masukkan nama anda");
+
+// confirm juga dapat dimasukkan ke dalam variabel. dimana bentuknya adalah boolean (true/false)
+var bol = confirm("Apakah anda yakin?")
+// menampilkan kalimat di dalam kurung di console
+console.log('Semangat Belajar Javascript');
+console.log(nama);
+console.log(bol);
